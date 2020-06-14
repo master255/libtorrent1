8eb0c224f3b087e13d4a4cb025787ee70f727d35
master255 <masters@inbox.ru>
2020-06-14 21:49:05 +0300

    start

--- include/libtorrent/torrent_handle.hpp
+++ include/libtorrent/torrent_handle.hpp
@@ -296,6 +296,8 @@ namespace aux {
 		// guaranteed to finish in the same order as you initiated them.
 		void read_piece(piece_index_t piece) const;
 
+		void add_part(std::int64_t start_byte, std::string save_path) const;
+
 		// Returns true if this piece has been completely downloaded and written
 		// to disk, and false otherwise.
 		bool have_piece(piece_index_t piece) const;
