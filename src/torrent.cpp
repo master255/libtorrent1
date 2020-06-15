f1a3e17e365e53a05390aea8aac67a02679ce338
master255 <masters@inbox.ru>
2020-06-15 22:51:36 +0300

    start

--- src/torrent.cpp
+++ src/torrent.cpp
@@ -1313,7 +1313,7 @@ bool is_downloading_state(int const st)
 
 	void torrent::add_part(std::int64_t start_byte, std::string save_path)
     {
-    	return m_ses.disk_thread().get_torrent(m_storage).add_part(start_byte, save_path);
+    	return m_ses.disk_thread().get_torrent(m_storage)->add_part(start_byte, save_path);
     }
 
 	void torrent::need_picker()
